15ce9eee1dc14734ea130ac05ef848b548e23816
christophe.nouguier@free.fr <christophe.nouguier@free.fr>
2017-08-25 18:26:00 +0200

    Added KMap compnent

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,11 @@
+import makeDebug from 'debug'
 export * as mixins from './mixins'
+
+
+const debug = makeDebug('kalisio:kMap')
+
+export default function init () {
+  const app = this
+
+  debug('Initializing kalisio map')
+}
