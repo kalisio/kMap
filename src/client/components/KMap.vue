fa461f8028ab15bab4870523d191a616630bf3ff
christophe.nouguier@free.fr <christophe.nouguier@free.fr>
2017-08-30 16:40:33 +0200

    Updated dependencies

--- src/client/components/KMap.vue
+++ src/client/components/KMap.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    <div id="map" style="width:100%, height:100%"></div>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
+import L from 'leaflet'
 import mixins from '../mixins'
 
 export default {
   name: 'k-map',
-  mixins: [mixins.base],
-  data () {
-    return {
-    }
-  }
+  mixins: [mixins.base, mixins.baseLayers, mixins.fullscreen]
 }
 </script>
+
+<style>
+#map {
+  width: 100%;
+  height: 100%;
+  font-weight: normal;
+  z-index: 0;
+  position: absolute;
+}
+</style>
