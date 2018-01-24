a49ebccb20fa807b71c1fa5a29cf8ff0cb94cd8f
Christophe Nouguier <christophe.nouguier@free.fr>
2018-01-24 22:38:06 +0100

    Fixed Colors import

--- src/client/components/KLocationMap.vue
+++ src/client/components/KLocationMap.vue
@@ -11,7 +11,7 @@ import _ from 'lodash'
 import 'leaflet-fa-markers/L.Icon.FontAwesome.css'
 import 'leaflet-fa-markers/L.Icon.FontAwesome.js'
 import * as mixins from '../mixins'
-import { mixins as kCoreMixins, Colors } from 'kCore/client'
+import { mixins as kCoreMixins, utils as kCoreUtils } from 'kCore/client'
 
 export default {
   name: 'k-location-map',
@@ -36,11 +36,9 @@ export default {
       type: Object,
       default: () => {
         return {
-			    iconClasses: 'fa fa-map-marker',
-			    markerColor: '#027be3',
-          iconColor: '#FFF',
-          iconXOffset: 2, 
-		      iconYOffset: 0,
+			    iconClasses: 'fa fa-circle',
+			    markerColor: kCoreUtils.Colors['blue'],
+          iconColor: '#FFF'
 		    }
       }
     }
