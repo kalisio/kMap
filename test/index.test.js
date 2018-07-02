ce16c62db68d20a1dfcb087138cb3462053693f8
Luc Claustres <luc.claustres@orange.fr>
2018-07-02 11:50:51 +0200

    Fixed tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,21 @@
 import chai, { util, expect } from 'chai'
 import chailint from 'chai-lint'
-import core, { kalisio } from 'kCore'
-import geocoder from '../src'
+import core, { kalisio, hooks, permissions } from 'kCore'
+import map, { permissions as mapPermissions } from '../src'
 
 describe('kMap', () => {
   let app, server, port, // baseUrl,
-    userService, authorisationService, geocoderService,
-    user1Object
+    userService, userObject, geocoderService
 
   before(() => {
     chailint(chai, util)
 
+    // Register all default hooks for authorisation
+    // Default rules for all users
+    permissions.defineAbilities.registerHook(permissions.defineUserAbilities)
+    // Then rules for maps
+    permissions.defineAbilities.registerHook(mapPermissions.defineMapAbilities)
+    
     app = kalisio()
     port = app.get('port')
     // baseUrl = `http://localhost:${port}${app.get('apiPath')}`
@@ -25,21 +30,21 @@ describe('kMap', () => {
     app.configure(core)
     userService = app.getService('users')
     expect(userService).toExist()
-    authorisationService = app.getService('authorisations')
-    expect(authorisationService).toExist()
-    app.configure(geocoder)
+    app.configure(map)
     geocoderService = app.getService('geocoder')
     expect(geocoderService).toExist()
     // Now app is configured launch the server
     server = app.listen(port)
     server.once('listening', _ => done())
   })
+  // Let enough time to process
+  .timeout(5000)
 
   it('creates a test user', () => {
-    return userService.create({ email: 'test-1@test.org', name: 'test-user-1' }, { checkAuthorisation: true })
+    return userService.create({ email: 'test-user@test.org', name: 'test-user' }, { checkAuthorisation: true })
     .then(user => {
-      user1Object = user
-      return userService.find({ query: { 'profile.name': 'test-user-1' }, checkAuthorisation: true })
+      userObject = user
+      return userService.find({ query: { 'profile.name': 'test-user' }, user: userObject, checkAuthorisation: true })
     })
     .then(users => {
       expect(users.data.length > 0).beTrue()
@@ -50,13 +55,28 @@ describe('kMap', () => {
 
   it('geocode an address', () => {
     let address = '80 chemin des tournesols, 11400 Castelnaudary'
-    geocoderService.create({ address: address }, { user: user1Object, checkAuthorisation: true })
+    geocoderService.create({ address: address }, { user: userObject, checkAuthorisation: true })
     .then(response => {
       expect(response.length === 1).beTrue()
       expect(response[0].latitude).toExist()
       expect(response[0].longitude).toExist()
     })
   })
+
+  it('removes the test user', () => {
+    return userService.remove(userObject._id, {
+      user: userObject,
+      checkAuthorisation: true
+    })
+    .then(user => {
+      return userService.find({ query: { name: 'test-user' } })
+    })
+    .then(users => {
+      expect(users.data.length === 0).beTrue()
+    })
+  })
+  // Let enough time to process
+  .timeout(5000)
 
   // Cleanup
   after(() => {
