67e40734d3dbf3a49c5a085c521b07dda2389906
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-09 10:36:58 +0100

    WIP #13 #12

---
+++ src/client/components/KMapPanel.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-list>
+    <q-collapsible icon="map" label="Base layers">
+      <div>
+        
+      </div>
+    </q-collapsible>
+    <q-collapsible icon="layers" label="Overlay layers">
+      <div>
+        
+      </div>
+    </q-collapsible>
+  </q-list>
+</template>
+
+<script>
+import { QList, QCollapsible } from 'quasar'
+
+export default {
+  name: 'k-map-panel',
+  components: {
+    QList,
+    QCollapsible
+  },
+  props: {
+    layers: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
