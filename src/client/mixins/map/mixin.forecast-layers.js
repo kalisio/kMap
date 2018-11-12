3b266399e6d7af7476358cc16125690836dd887a
Luc Claustres <luc.claustres@orange.fr>
2018-11-12 18:15:52 +0100

    Improved forecast layers mixin

--- src/client/mixins/map/mixin.forecast-layers.js
+++ src/client/mixins/map/mixin.forecast-layers.js
@@ -4,10 +4,26 @@ import 'iso8601-js-period/iso8601.js'
 let forecastLayersMixin = {
   data () {
     return {
-      currentTime: null
+      currentTime: null,
+      forecastModel: null
     }
   },
   methods: {
+    getVisualModel (layerConfig) {
+      // For visualization we might decimate the data resolution for performance reasons
+      let decimationFactor = layerConfig.decimationFactor || 2
+      // Copy forecast model
+      let visualModel = Object.assign({}, this.forecastModel)
+      // Then assign visual configuration
+      Object.assign(visualModel, {
+        name: this.forecastModel.name,
+        origin: this.forecastModel.origin,
+        bounds: this.forecastModel.bounds,
+        size: [Math.floor(this.forecastModel.size[0] / decimationFactor), Math.floor(this.forecastModel.size[1] / decimationFactor)],
+        resolution: [decimationFactor * this.forecastModel.resolution[0], decimationFactor * this.forecastModel.resolution[1]],
+      })
+      return visualModel
+    },
     setupForecastModels () {
       return this.weacastApi.getService('forecasts').find()
       .then(response => {
@@ -21,23 +37,15 @@ let forecastLayersMixin = {
       if (!this.forecastModel || !this.map) return
 
       this.$config('map.forecastLayers').forEach(layerConfig => {
-        let layer = new L.Weacast[layerConfig.type](this.weacastApi, layerConfig.options)
-        this.addLayer(layerConfig.name, layer)
+        let layer = this.getLayerByName(layerConfig.name)
+        // Need to be created first
+        if (!layer) {
+          layer = new L.Weacast[layerConfig.type](this.weacastApi, layerConfig.options)
+          this.addLayer(layerConfig.name, layer)
+          this.forecastLayers.push(layer)
+        }
         // For visualization we might decimate the data resolution for performance reasons
-        let decimationFactor = layerConfig.decimationFactor || 2
-        // Copy forecast model
-        let visualModel = Object.assign({}, this.forecastModel)
-        // Then assign visual configuration
-        Object.assign(visualModel, {
-          name: this.forecastModel.name,
-          origin: this.forecastModel.origin,
-          bounds: this.forecastModel.bounds,
-          size: [Math.floor(this.forecastModel.size[0] / decimationFactor), Math.floor(this.forecastModel.size[1] / decimationFactor)],
-          resolution: [decimationFactor * this.forecastModel.resolution[0], decimationFactor * this.forecastModel.resolution[1]],
-        })
-        // Should come last so that we do not trigger multiple updates of data
-        layer.setForecastModel(visualModel)
-        this.forecastLayers.push(layer)
+        layer.setForecastModel(this.getVisualModel(layerConfig))
       })
     }
   },
