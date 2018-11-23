579bfcb907aafee5d4a41e80b2f39ae9db2845ac
Luc Claustres <luc.claustres@orange.fr>
2018-11-23 18:15:17 +0100

    WIP https://github.com/kalisio/kMap/issues/20 (added style generators, layer options, etc. for Leaflet)

--- src/client/mixins/map/mixin.geojson-layers.js
+++ src/client/mixins/map/mixin.geojson-layers.js
@@ -19,7 +19,7 @@ let geojsonLayersMixin = {
           // Alter type as required by the plugin
           options.type = 'realtime'
           const id = _.get(layerOptions, 'id')
-          if (id) _.set(layerOptions, 'getFeatureId', (feature) => _.get(feature, id))
+          if (id) _.set(layerOptions, 'getFeatureId', (feature) => _.get(feature, 'properties.' + id))
           container = _.get(layerOptions, 'container')
           if (container) {
             layerOptions.container = container = this.createLeafletLayer({ type: container, arguments: [] })
@@ -30,7 +30,7 @@ let geojsonLayersMixin = {
           container = this.createLeafletLayer({ type: 'markerClusterGroup', arguments: [ layerOptions.cluster ] })
         }
         // Merge generic GeoJson options and layer options
-        let geoJsonOptions = this.getGeoJsonOptions()
+        let geoJsonOptions = this.getGeoJsonOptions(layerOptions)
         Object.keys(geoJsonOptions).forEach(key => {
           // If layer provided do not override
           if (!_.has(layerOptions, key)) layerOptions[key] = geoJsonOptions[key]
@@ -83,108 +83,162 @@ let geojsonLayersMixin = {
       }
     },
     convertFromSimpleStyleSpec (style) {
+      if (!style) return {}
       let convertedStyle = {}
       const mappings = {
         'stroke': 'color',
         'stroke-opacity': 'opacity',
         'stroke-width': 'weight',
         'fill-opacity': 'fillOpacity',
-        'fill-color': 'fillColor'
+        'fill-color': 'fillColor',
+        'marker-size': 'icon.options.iconSize',
+        'marker-symbol': 'icon.options.iconUrl',
+        'marker-color': 'icon.options.markerColor',
+        'icon-color': 'icon.options.iconColor',
+        'icon-anchor': 'icon.options.iconAnchor',
+        'icon-classes': 'icon.options.iconClasses'
       }
       _.forOwn(style, (value, key) => {
         const mapping = _.get(mappings, key)
-        if (mapping) _.set(convertedStyle, mapping, value)
+        if (mapping) {
+          // Specific options
+          switch (key) {
+            case 'icon-anchor':
+            case 'marker-size':
+              if (!Array.isArray(value)) value = [value, value]
+            default:
+              _.set(convertedStyle, mapping, value)
+          }
+          // In this case we have a marker spec
+          if (key.startsWith('icon') || key.startsWith('marker')) {
+            _.set(convertedStyle, 'icon.type', _.has(style, 'icon-classes') ? 'icon.fontAwesome' : 'icon')
+            _.set(convertedStyle, 'type', 'marker')
+          }
+        }
       })
-
       return convertedStyle
     },
-    getGeoJsonOptions () {
+    registerLeafletStyle(type, generator) {
+      this[type + 'Factory'].push(generator)
+    },
+    generateLeafletStyle() {
+      let args = Array.from(arguments)
+      const type = args[0]
+      args.shift()
+      let style
+      // Iterate over all registered generators until we find one
+      for (let i = 0; i < this[type + 'Factory'].length; i++) {
+        const generator = this[type + 'Factory'][i]
+        style = generator(...args)
+        if (style) break
+      }
+      return style
+    },
+    getPointMarker (layerOptions, feature, latlng) {
+      let style = this.generateLeafletStyle('markerStyle', feature, latlng)
+      return style || // Feature style overrides layer style which overrides default style
+        this.createMarkerFromStyle(latlng, Object.assign({}, this.options.pointStyle,
+        this.convertFromSimpleStyleSpec(layerOptions),
+        this.convertFromSimpleStyleSpec(feature.style || feature.properties)))
+    },
+    getFeatureStyle (layerOptions, feature) {
+      let style = this.generateLeafletStyle('featureStyle', feature)
+      return style || // Feature style overrides layer style which overrides default style
+        Object.assign({}, this.options.featureStyle || {
+          opacity: 1,
+          radius: 6,
+          color: 'red',
+          fillOpacity: 0.5,
+          fillColor: 'green'
+        },
+        this.convertFromSimpleStyleSpec(layerOptions),
+        this.convertFromSimpleStyleSpec(feature.style || feature.properties))
+    },
+    getFeaturePopup (layerOptions, feature, layer) {
+      let popup = this.generateLeafletStyle('popup', feature, layer)
+      if (!popup && feature.properties) {
+        const popupStyle = layerOptions.popup || this.options.popup
+        // Default content
+        let properties = feature.properties
+        // Custom list given ?
+        if (popupStyle) {
+          if (popupStyle.pick) {
+            properties = _.pick(properties, popupStyle.pick)
+          } else if (popupStyle.omit) {
+            properties = _.omit(properties, popupStyle.omit)
+          }
+        }
+        properties = _.pickBy(properties, value => !_.isNil(value))
+        const keys = _.keys(properties)
+        let html
+        if (keys.length === 0) return null
+        else if (keys.length === 1) html = _.get(properties, keys[0])
+        else {
+          const borderStyle = ' style="border: 1px solid black; border-collapse: collapse;"'
+          html = '<table' + borderStyle + '>'
+          html += '<tr' + borderStyle + '><th' + borderStyle + '>Property</th><th>Value</th></tr>'
+          html += keys
+            .map(key => '<tr style="border: 1px solid black; border-collapse: collapse;"><th' +
+              borderStyle + '>' + key + '</th><th>' + _.get(properties, key) + '</th></tr>')
+            .join('')
+          html += '</table>'
+        }
+        // Configured or default style
+        if (popupStyle && popupStyle.options) {
+          popup = L.popup(popupStyle.options, layer)
+        } else {
+          popup = L.popup({
+            maxHeight: 400,
+            maxWidth: 400,
+            autoPan: false
+          }, layer)
+        }
+        popup.setContent(html)
+      }
+      return popup
+    },
+    getFeatureTooltip (layerOptions, feature, layer) {
+      let tooltip = this.generateLeafletStyle('tooltip', feature, layer)
+      if (!tooltip) {
+        const tooltipStyle = layerOptions.tooltip || this.options.tooltip
+        if (tooltipStyle && feature.properties) {
+          // Default content
+          let properties = feature.properties
+          let html
+          if (tooltipStyle.property) {
+            html = _.get(properties, tooltipStyle.property)
+          } else if (tooltipStyle.template) {
+            let compiler = _.template(tooltipStyle.template)
+            html = compiler(properties)
+          }
+          if (html) {
+            tooltip = L.tooltip(tooltipStyle.options || { permanent: false }, layer)
+            tooltip.setContent(html)
+          }
+        }
+      }
+      return tooltip
+    },
+    getGeoJsonOptions (layerOptions = {}) {
       let geojsonOptions = {
         onEachFeature: (feature, layer) => {
-          const featureStyle = this.options.featureStyle
-          let popup
-          // Custom defined function in component for popup ?
-          if (typeof this.getFeaturePopup === 'function') {
-            popup = this.getFeaturePopup(feature, layer)
-          }
-          if (!popup && feature.properties) {
-            // Default content
-            const borderStyle = ' style="border: 1px solid black; border-collapse: collapse;"'
-            let html = '<table' + borderStyle + '>'
-            html += '<tr' + borderStyle + '><th' + borderStyle + '>Property</th><th>Value</th></tr>'
-            let properties = Object.keys(feature.properties)
-            // Custom list given ?
-            if (featureStyle && featureStyle.popup) {
-              if (featureStyle.popup.properties) {
-                properties = featureStyle.popup.properties
-              }
-              if (featureStyle.popup.excludedProperties) {
-                properties = properties.filter(property => !featureStyle.popup.excludedProperties.includes(property))
-              }
-            }
-            html += properties
-            .filter(k => feature.properties[k] !== null && feature.properties[k] !== undefined)
-            .map(k => '<tr style="border: 1px solid black; border-collapse: collapse;"><th' + borderStyle + '>' + k + '</th><th>' + feature.properties[k] + '</th></tr>')
-            .join('')
-            html += '</table>'
-            // Configured or default style
-            if (featureStyle && featureStyle.popup && featureStyle.popup.options) {
-              layer.bindPopup(html, featureStyle.popup.options)
-              popup = L.popup(featureStyle.popup.options, layer)
-            } else {
-              popup = L.popup({
-                maxHeight: 400,
-                maxWidth: 400
-              }, layer)
-            }
-            popup.setContent(html)
-          }
+          let popup = this.getFeaturePopup(layerOptions, feature, layer)
           if (popup) {
             layer.bindPopup(popup)
             bindLeafletEvents(layer.getPopup(), LeafletEvents.Popup, this)
           }
 
-          let tooltip
-          // Custom defined function in component for tooltip ?
-          if (typeof this.getFeatureTooltip === 'function') {
-            tooltip = this.getFeatureTooltip(feature, layer)
-          } else if (featureStyle && featureStyle.tooltip && featureStyle.tooltip.property && feature.properties) {
-            tooltip = L.tooltip(featureStyle.tooltip.options || { permanent: false }, layer)
-            tooltip.setContent(feature.properties[featureStyle.tooltip.property])
-          }
+          let tooltip = this.getFeatureTooltip(layerOptions, feature, layer)
           if (tooltip) {
             layer.bindTooltip(tooltip)
             bindLeafletEvents(layer.getTooltip(), LeafletEvents.Tooltip, this)
           }
         },
         style: (feature) => {
-          let style
-          // Custom defined function in component ?
-          if (typeof this.getFeatureStyle === 'function') {
-            style = this.getFeatureStyle(feature)
-          }
-          if (!style) {
-            // Feature style overrides default style
-            style = Object.assign({}, this.options.featureStyle || {
-              opacity: 1,
-              radius: 6,
-              color: 'red',
-              fillOpacity: 0.5,
-              fillColor: 'green'
-            }, this.convertFromSimpleStyleSpec(feature.properties || {}))
-          }
-          return style
+          return this.getFeatureStyle(layerOptions, feature)
         },
         pointToLayer: (feature, latlng) => {
-          let marker
-          // Custom defined function in component ?
-          if (typeof this.getPointMarker === 'function') {
-            marker = this.getPointMarker(feature, latlng)
-          }
-          if (!marker) {
-            // Configured or default style
-            marker = this.createMarkerFromStyle(latlng, this.options.pointStyle)
-          }
+          let marker = this.getPointMarker(layerOptions, feature, latlng)
           if (marker) bindLeafletEvents(marker, LeafletEvents.Marker, this)
           return marker
         }
@@ -194,6 +248,10 @@ let geojsonLayersMixin = {
     }
   },
   created () {
+    this.tooltipFactory = []
+    this.popupFactory = []
+    this.markerStyleFactory = []
+    this.featureStyleFactory = []
     this.registerLeafletConstructor(this.createLeafletGeoJsonLayer)
   }
 }
