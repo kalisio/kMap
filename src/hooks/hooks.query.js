263edbf7f126a9ecdc9111f9ba2cf48a43e4ea33
Luc Claustres <luc.claustres@orange.fr>
2018-11-25 22:48:24 +0100

    Fixed typo

--- src/hooks/hooks.query.js
+++ src/hooks/hooks.query.js
@@ -12,23 +12,7 @@ export function marshallGeometryQuery (hook) {
 export function marshallSpatialQuery (hook) {
   let query = hook.params.query
   if (query) {
-    if (!_.isNil(query.geomtry)) marshallGeometry(query.geometry)
-    // Resampling is used by hooks only, do not send it to DB
-    if (!_.isNil(query.oLon) && !_.isNil(query.oLat) && !_.isNil(query.sLon) && !_.isNil(query.sLat) && !_.isNil(query.dLon) && !_.isNil(query.dLat)) {
-      // Convert when required from query strings
-      hook.params.oLat = _.toNumber(query.oLat)
-      hook.params.oLon = _.toNumber(query.oLon)
-      hook.params.sLat = _.toNumber(query.sLat)
-      hook.params.sLon = _.toNumber(query.sLon)
-      hook.params.dLat = _.toNumber(query.dLat)
-      hook.params.dLon = _.toNumber(query.dLon)
-      delete query.oLat
-      delete query.oLon
-      delete query.sLat
-      delete query.sLon
-      delete query.dLat
-      delete query.dLon
-    }
+    if (!_.isNil(query.geometry)) marshallGeometry(query.geometry)
     // Shortcut for proximity query
     if (!_.isNil(query.centerLon) && !_.isNil(query.centerLat) && !_.isNil(query.distance)) {
       let lon = _.toNumber(query.centerLon)
