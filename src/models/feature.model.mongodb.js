163a2e642ebe49b32293093a24502e3d600d532e
Luc Claustres <luc.claustres@orange.fr>
2018-11-28 21:22:57 +0100

    Fixed lint

--- src/models/feature.model.mongodb.js
+++ src/models/feature.model.mongodb.js
@@ -2,12 +2,12 @@ module.exports = function (app, options) {
   let db = options.db || app.db
   options.Model = db.collection(options.collection)
   if (options.history) {
-  	options.Model.createIndex({ time: 1 }, { expireAfterSeconds: options.history })
+    options.Model.createIndex({ time: 1 }, { expireAfterSeconds: options.history })
   } else {
-  	options.Model.createIndex({ time: 1 })
+    options.Model.createIndex({ time: 1 })
   }
   options.Model.createIndex({ geometry: '2dsphere' })
   if (options.featureId) {
-  	options.Model.createIndex({ ['properties.' + options.featureId]: 1 })
+    options.Model.createIndex({ ['properties.' + options.featureId]: 1 })
   }
 }
