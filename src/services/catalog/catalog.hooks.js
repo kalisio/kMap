0725d2ff89dc90b9500652a1a14bb21fceb5e357
Luc Claustres <luc.claustres@orange.fr>
2018-12-04 22:53:49 +0100

    WIP https://github.com/kalisio/kMap/issues/32 (added weacast + filter hook)

--- src/services/catalog/catalog.hooks.js
+++ src/services/catalog/catalog.hooks.js
@@ -1,7 +1,21 @@
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [ hook => {
+      let query = hook.params.query || {}
+      // Filter objects according to target type (either layers or services)
+      if (query.type) {
+        switch (query.type) {
+          case 'service':
+            // Nothing special todo
+            break
+          case 'layer':
+          default:
+            query.type = { $ne: 'service' }
+            break
+        }
+      }
+    }],
     get: [],
     create: [],
     update: [],
