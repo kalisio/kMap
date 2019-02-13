b6c48def45b178291afe86aeb338ca9938053988
Luc Claustres <luc.claustres@orange.fr>
2019-02-13 21:34:34 +0100

    WIP https://github.com/kalisio/kMap/issues/10 (support multiple providers)

--- src/services/geocoder/geocoder.service.js
+++ src/services/geocoder/geocoder.service.js
@@ -1,3 +1,5 @@
+import logger from 'winston'
+import _ from 'lodash'
 import NodeGeocoder from 'node-geocoder'
 import makeDebug from 'debug'
 
@@ -7,11 +9,29 @@ export default function (name, app, options) {
     // Keep track of config
   Object.assign(options, app.get('geocoder'))
   debug('geocoder created with config ', options)
-  const geocoder = NodeGeocoder(options)
+  let geocoders = []
+  // Single provider ?
+  if (options.providers) {
+    geocoders = options.providers.map(geocoderOptions => NodeGeocoder(geocoderOptions))
+  } else {
+    geocoders.push(NodeGeocoder(options))
+  }
   return {
-    create (data, params) {
+    async geocode(geocoder, address) {
+      let results = []
+      try {
+        results = await geocoder.geocode(address)
+      } catch (error) {
+        logger.error(error)
+      }
+      return results
+    },
+    async create (data, params) {
       debug('geocoder service called for create', data)
-      return geocoder.geocode(data.address)
+      let results = await Promise.all(geocoders.map(geocoder => this.geocode(geocoder, data.address)))
+      // FIXME: we should order according to best matching
+      results = _.flatten(results)
+      return results
     }
   }
 }
