5dd8db0e2925411768cc4767f71af193b9b8ac62
Luc Claustres <luc.claustres@orange.fr>
2019-03-29 14:02:48 +0100

    fix: editing a layer just after saving fails Fixes https://github.com/kalisio/kano/issues/62

--- src/client/mixins/map/mixin.edit-layers.js
+++ src/client/mixins/map/mixin.edit-layers.js
@@ -10,7 +10,7 @@ let editLayersMixin = {
     isLayerEdited (name) {
       return this.editedLayer && (this.editedLayer.name === name)
     },
-    editLayer (name) {
+    async editLayer (name) {
       let options = this.getLayerByName(name)
       let leafletLayer = this.getLeafletLayerByName(name)
       if (!options || !leafletLayer) return
@@ -19,26 +19,25 @@ let editLayersMixin = {
         // Remove UI
         this.map.removeControl(this.editControl)
         this.editControl = null
-        // Set back edited layers to source layer, use bulk add if it exists
-        if (typeof leafletLayer.addLayers === 'function') leafletLayer.addLayers(this.editableLayers.getLayers())
-        else this.editableLayers.getLayers().forEach(layer => leafletLayer.addLayer(layer))
-        this.editableLayers.clearLayers()
-        this.map.removeLayer(this.editableLayers)
+        // Set back edited layers to source layer
+        this.map.removeLayer(this.editableLayer)
+        leafletLayer.addLayer(this.editableLayer)
         this.editedLayer = null
+        this.editedLayerSchema = null
         this.deleteInProgress = false
       } else { // Start edition
         this.editedLayer = options
         // Move source layers to edition layers, required as eg clusters are not supported
         let geoJson = leafletLayer.toGeoJSON()
         leafletLayer.clearLayers()
-        this.editableLayers = new L.geoJson(geoJson, this.getGeoJsonOptions(options))
-        this.map.addLayer(this.editableLayers)
+        this.editableLayer = new L.geoJson(geoJson, this.getGeoJsonOptions(options))
+        this.map.addLayer(this.editableLayer)
         // Add UI
         this.editControl = new L.Control.Draw({
           position: 'bottomleft',
           draw: this.editOptions,
           edit: {
-            featureGroup: this.editableLayers,
+            featureGroup: this.editableLayer,
             remove: true
           }
         })
@@ -46,37 +45,50 @@ let editLayersMixin = {
         this.createdFeatures = []
         this.editedFeatures = []
         this.deletedFeatures = []
+        this.editedLayerSchema = await this.loadLayerSchema(this.editedLayer)
       }
     },
-    async onUpdateFeatureProperties (layer, event) {
-      const feature = _.get(event, 'target.feature')
-      if (!feature || !this.isLayerEdited(layer.name)) return
-      if (!layer.schema) return // No edition schema
-      // Check if not currently in the edition workspace for removal
-      if (this.deleteInProgress) return
-      // Avoid default popup
-      const popup = event.target.getPopup()
-      if (popup) event.target.unbindPopup(popup)
+    async loadLayerSchema (layer) {
+      if (!layer.schema) return null
       const data = await this.$api.getService('storage', '').get(`schemas/${layer._id}.json`)
       if (!data.uri) throw Error(this.$t('errors.CANNOT_PROCESS_SCHEMA_DATA'))
       const typeAndData = data.uri.split(',')
       if (typeAndData.length <= 1) throw Error(this.$t('errors.CANNOT_PROCESS_SCHEMA_DATA'))
       // We get data as a data URI
-      const schema = atob(typeAndData[1])
+      return atob(typeAndData[1])
+    },
+    async onUpdateFeatureProperties (layer, event) {
+      const leafletLayer = event.target
+      if (!leafletLayer) return
+      let feature = _.get(leafletLayer, 'feature')
+      if (!feature || !this.isLayerEdited(layer.name)) return
+      if (!this.editedLayerSchema) return // No edition schema
+      // Check if not currently in the edition workspace for removal
+      if (this.deleteInProgress) return
+      // Avoid default popup
+      const popup = leafletLayer.getPopup()
+      if (popup) leafletLayer.unbindPopup(popup)
+      
       this.editFeatureModal = await this.$createComponent('editor/KModalEditor', {
         propsData: {
           service: 'features',
           objectId: feature._id,
-          schemaJson: schema,
+          schemaJson: this.editedLayerSchema,
           perspective: 'properties'
         }
       })
       this.editFeatureModal.$mount()
       this.editFeatureModal.open()
       this.editFeatureModal.$on('applied', async updatedFeature => {
+        const updatedProperties = _.pick(updatedFeature, ['properties'])
         // Restore popup
-        if (popup) event.target.bindPopup(popup)
-        await this.$api.getService('features').patch(feature._id, _.pick(updatedFeature, ['properties']))
+        if (popup) leafletLayer.bindPopup(popup)
+        // Save in DB and in memory
+        await this.$api.getService('features').patch(feature._id, updatedProperties)
+        let geoJson = leafletLayer.toGeoJSON()
+        Object.assign(geoJson, updatedProperties)
+        this.editableLayer.removeLayer(leafletLayer)
+        this.editableLayer.addData(geoJson)
         this.editFeatureModal.close()
         this.editFeatureModal = null
       })
@@ -86,7 +98,7 @@ let editLayersMixin = {
       // Save changes to DB, we use the layer DB ID as layer ID on features
       geoJson.layer = this.editedLayer._id
       geoJson = await this.$api.getService('features').create(geoJson)
-      this.editableLayers.addData(geoJson)
+      this.editableLayer.addData(geoJson)
     },
     async onFeaturesEdited (event) {
       const geoJson = event.layers.toGeoJSON()
@@ -94,7 +106,7 @@ let editLayersMixin = {
       // Save changes to DB
       for (let i = 0; i < features.length; i++) {
         const feature = features[i]
-        await this.$api.getService('features').patch(feature._id, _.pick(feature, ['properties', 'geometry']))
+        await this.$api.getService('features').patch(feature._id, _.pick(feature, ['geometry']))
       }
     },
     async onFeaturesDeleted (event) {
