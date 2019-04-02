82c4ec163f36e96db4c843c1b4cc6afb93a9facf
Luc Claustres <luc.claustres@orange.fr>
2019-04-02 16:38:52 +0200

    Fixed import of removed mixins

--- src/client/mixins/map/index.js
+++ src/client/mixins/map/index.js
@@ -1,8 +1,6 @@
 import baseMap from './mixin.base-map'
 import geojsonLayers from './mixin.geojson-layers'
 import fileLayers from './mixin.file-layers'
-import serviceLayers from './mixin.service-layers'
-import collectionLayer from './mixin.collection-layer'
 import forecastLayers from './mixin.forecast-layers'
 import geotiffLayers from './mixin.geotiff-layers'
 import editLayers from './mixin.edit-layers'
@@ -11,8 +9,6 @@ export default {
   baseMap,
   geojsonLayers,
   fileLayers,
-  serviceLayers,
-  collectionLayer,
   forecastLayers,
   geotiffLayers,
   editLayers
