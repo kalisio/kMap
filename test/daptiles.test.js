95ffe75a0e164b917542acf7febc7875d7385156
Robin Bourianes <robin.bourianes@frizko.com>
2019-10-07 14:00:52 +0200

    daptiles test wip ...

--- test/daptiles.test.js
+++ test/daptiles.test.js
@@ -1,24 +1,27 @@
+import chai, { util, expect } from 'chai'
+import chailint from 'chai-lint'
 import core, { kalisio, hooks } from '@kalisio/kdk-core'
-import map from '../src'
+// import map from '../src'
 import distribution from '@kalisio/feathers-distributed'
 
 describe('kMap:daptiles', () => {
-    let app, server, port, daptilesService
+    let app, server, port, baseUrl, daptilesService
 
     before(() => {
+        chailint(chai, util)
         app = kalisio()
         // Distribute services
-        app.configure(distribution(app.get('distribution').app))
+        //app.configure(distribution(app.get('distribution').app))
         // Register log hook
-        app.hooks({
-            error: { all: hooks.log }
-        })
+        app.hooks({ error: { all: hooks.log } })
         port = app.get('port')
+        baseUrl = `http://localhost:${port}${app.get('apiPath')}`
+        return app.db.connect()
     })
 
     it('registers the daptiles service', (done) => {
         app.configure(core)
-        app.configure(map)
+        // app.configure(map)
         daptilesService = app.getService('daptiles')
         expect(daptilesService).toExist()
        
