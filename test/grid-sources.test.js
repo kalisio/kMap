1e3c68592d24ffd4ab2ba04e4920ab4283f7f5fc
Luc Claustres <luc.claustres@orange.fr>
2019-12-10 15:18:31 +0100

    feat: Created a Weacast grid source for tiled mesh layer closes https://github.com/kalisio/kMap/issues/128 (added tests)

--- test/grid-sources.test.js
+++ test/grid-sources.test.js
@@ -1,7 +1,14 @@
 import chai, { util, expect } from 'chai'
 import chailint from 'chai-lint'
 import fs from 'fs'
+import _ from 'lodash'
 import nock from 'nock'
+import sift from 'sift'
+import moment from 'moment'
+import feathers from '@feathersjs/feathers'
+import memory from 'feathers-memory'
+import intersect from '@turf/intersect'
+import { weacast, createElementService } from 'weacast-core'
 import { makeGridSource } from '../src/common/grid'
 import * as wcs from '../src/common/wcs-utils'
 
@@ -180,9 +187,96 @@ describe('kMap:grid-source', () => {
     })
   })
 
-  /*
-    after(() => {
+  describe('weacast', () => {
+    const model = { name: 'gfs-world', interval: 3 * 3600, bounds: [0, -90, 360, 90] }
+    const element = { name: 'gust' }
+    const service = `${model.name}/${element.name}`
+    let weacastOptions = {
+      weacast: { element: element.name, model }
+    }
 
+    const store = {
+      // Raw data
+      0: {
+        forecastTime: moment.utc('2019-01-04T00:00:00.000Z').toDate(),
+        minValue: -20, maxValue: 20, data: new Array(720*361).fill(0)
+      },
+      // Tiles
+      1: {
+        forecastTime: moment.utc('2019-01-04T00:00:00.000Z').toDate(),
+        minValue: -5, maxValue: 20, data: new Array(40*40).fill(0),
+        geometry: {
+          type: 'Polygon',
+          coordinates: [ [ [0, 70], [20, 70], [20, 90], [0, 90], [0 ,70] ] ]
+        },
+        x: 0, y: 0, bounds: [0, 70, 20, 90], size: [40, 40]
+      },
+      2: {
+        forecastTime: moment.utc('2019-01-04T00:00:00.000Z').toDate(),
+        minValue: -20, maxValue: 5, data: new Array(40*40).fill(1),
+        geometry: {
+          type: 'Polygon',
+          coordinates: [ [ [20, 70], [40, 70], [40, 90], [20, 90], [20 ,70] ] ]
+        },
+        x: 0, y: 0, bounds: [20, 70, 40, 90], size: [40, 40]
+      }
+    }
+
+    it('initialize Weacast API mock', () => {
+      // Add geospatial operator to sift
+      const matcher = (query) => sift(query, {
+        expressions: {
+          $geoIntersects: function(query, value) {
+            const polygon1 = _.get(query, '$geometry')
+            const polygon2 = value
+            if (!polygon1 || !polygon2) return false
+            return intersect(polygon1, polygon2)
+          }
+        }
+      })
+      const weacastApi = weacast()
+      weacastApi.createElementService(model, element, memory({ store, matcher }))
+      const elementService = weacastApi.getService(service)
+      expect(elementService).toExist()
+      weacastOptions.weacast.api = weacastApi
     })
-  */
+
+    it('is possible to create a Weacast source from makeGridSource', () => {
+      const ret = makeGridSource(weacastOptions)
+      source = ret[0]
+      sourceOptions = ret[1]
+      expect(source).to.exist
+      expect(sourceOptions).to.deep.equal(weacastOptions.weacast)
+    })
+    
+    it('setup correctly', async () => {
+      source.setCurrentTime(moment.utc('2019-01-04T01:25:00.000Z'))
+      await source.setup(sourceOptions)
+      const bbox = source.getBBox()
+      expect(bbox[0]).to.be.equal(-90.0)
+      expect(bbox[1]).to.be.equal(-180.0)
+      expect(bbox[2]).to.be.equal(90.0)
+      expect(bbox[3]).to.be.equal(180.0)
+      const minmax = source.getDataBounds()
+      expect(minmax).to.deep.equal([-20, 20])
+    })
+    
+    it('returns an appropriate grid when requesting data', async () => {
+      const fetchBBox = [ 80, 10, 85, 30 ]
+      const fetchRes = [ 0.5, 0.5 ]
+      const grid = await source.fetch(null, fetchBBox, fetchRes)
+      const bbox = grid.getBBox()
+      expect(bbox).to.satisfy((bbox) => contains(bbox, fetchBBox))
+      // Check tiles are correctly managed
+      let value = grid.getValue(20, 20)
+      expect(value).to.be.equal(0)
+      value = grid.getValue(20, 60)
+      expect(value).to.be.equal(1)
+    })
+    
+  })
+
+  after(() => {
+    // Nothing to clean up
+  })
 })
