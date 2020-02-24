c5812e041c1a33330717ebfe525a1a0f25d3a59b
Luc Claustres <luc.claustres@orange.fr>
2020-02-24 23:31:33 +0100

    Fixed heatmap update

--- src/client/mixins/map/mixin.heatmap-layers.js
+++ src/client/mixins/map/mixin.heatmap-layers.js
@@ -9,29 +9,24 @@ export default {
       const layer = this.createLeafletLayer(options)
       return layer
     },
-    updateHeatmap (name, geoJsonOrConfig) {
+    updateHeatmap (name, geoJson) {
       // Retrieve the layer
       let layer = this.getLeafletLayerByName(name)
       if (!layer) return // Cannot update invisible layer
-      // If no features we assume a config object
-      if (!geoJsonOrConfig.features) {
-        layer._heatmap.configure(geoJsonOrConfig)
-      } else {
-        // Otherwise we update data
-        const valueField = _.get(layer, 'options.valueField')
-        const values = (valueField ? geoJsonOrConfig.features.map(feature => _.get(feature, valueField)): [])
-        // By default our intensity is based on the number of points only
-        // otherwise when provided we use target value
-        layer.setData({
-          min: (valueField ? _.min(values) : 0),
-          max: (valueField ? _.max(values) : 1),
-          data: geoJsonOrConfig.features.map(feature => ({
-            lng: _.get(feature, 'geometry.coordinates[0]'),
-            lat: _.get(feature, 'geometry.coordinates[1]'),
-            value: (valueField ? _.get(feature, valueField) : 1)
-          }))
-        })
-      }
+      // Otherwise we update data
+      const valueField = _.get(layer, 'options.valueField')
+      const values = (valueField ? geoJson.features.map(feature => _.get(feature, valueField)): [])
+      // By default our intensity is based on the number of points only
+      // otherwise when provided we use target value
+      layer.setData({
+        min: (valueField ? _.min(values) : 0),
+        max: (valueField ? _.max(values) : 1),
+        data: geoJson.features.map(feature => ({
+          lng: _.get(feature, 'geometry.coordinates[0]'),
+          lat: _.get(feature, 'geometry.coordinates[1]'),
+          value: (valueField ? _.get(feature, valueField) : 1)
+        }))
+      })
     }
   },
   created () {
