4561574bc5a8fe652ebb1a186805e5224880fdad
Luc Claustres <luc.claustres@orange.fr>
2020-02-27 15:47:51 +0100

    Improved feature chart

--- src/client/components/KFeaturesChart.vue
+++ src/client/components/KFeaturesChart.vue
@@ -1,16 +1,24 @@
 <template>
   <k-modal ref="modal" :title="title" :toolbar="toolbar" :buttons="[]" >
     <div slot="modal-content">
-      <div class="row justify-center">
-        <q-select class="col-2" v-model="property" :label="$t('KFeaturesChart.PROPERTY_LABEL')" stack-label
+      <div class="row justify-center text-center">
+        <q-select class="col-1" v-model="property" :label="$t('KFeaturesChart.PROPERTY_LABEL')" stack-label
           :options="properties" @input="refreshChart"/>
-        <q-select class="col-2" v-model="chartType" :label="$t('KFeaturesChart.CHART_LABEL')" stack-label
-          :options="chartTypes" @input="refreshChart"/>
-        <q-select class="col-2" v-model="nbValuesPerChart" :label="$t('KFeaturesChart.PAGINATION_LABEL')" stack-label
-          :options="paginationOptions" @input="refreshChart" emit-value/>
+        <q-select class="col-1" v-model="chartType" :label="$t('KFeaturesChart.CHART_LABEL')" stack-label
+          :options="chartOptions" @input="refreshChart"/>
+        <q-select class="col-1" v-model="nbValuesPerChart" :label="$t('KFeaturesChart.PAGINATION_LABEL')" stack-label
+          :options="paginationOptions" @input="refreshChartAndPagination"/>
+        <q-select class="col-1" v-model="render" :label="$t('KFeaturesChart.RENDER_LABEL')" stack-label
+          :options="renderOptions" @input="refreshChart"/>
         <q-pagination v-if="nbCharts > 1" v-model="currentChart" :max="nbCharts" @input="refreshChart" :input="true"/>
       </div>
-      <canvas ref="chart"></canvas>
+      <div class="row justify-center text-center">
+        <div v-show="chartData.length === 0" class="q-ma-xl">
+          <q-icon size="3rem" name="insert_chart_outlined" />
+          <span class="text-h5">{{$t('KFeaturesChart.SELECT_PROPERTY_LABEL')}}</span>
+        </div>
+        <canvas v-show="chartData.length > 0" class="chart q-ma-lg" ref="chart"></canvas>
+      </div>
     </div>
   </k-modal>
 </template>
@@ -50,32 +58,43 @@ export default {
         // Check if we have a translation key or directly the label content
         properties.push({ value: key, label: (this.$i18n.i18next.exists(label) ? this.$t(label) : label) })
       })
-      if (properties.length) this.property = properties[0]
+      //if (properties.length) this.property = properties[0]
       return properties
     },
     nbCharts () {
-      if (!this.chartData.length || (this.nbValuesPerChart === 0)) return 1
-      else return Math.ceil(this.chartData.length / this.nbValuesPerChart)
+      if (!this.chartData.length || (this.nbValuesPerChart.value === 0)) return 1
+      else return Math.ceil(this.chartData.length / this.nbValuesPerChart.value)
     }
   },
   data () {
+    const chartTypes = ['pie', 'polarArea', 'radar', 'bar']
+    const chartOptions = chartTypes.map(
+        type => ({ value: type, label: this.$i18n.t(`KFeaturesChart.CHART_LABEL_${type.toUpperCase()}`) }))
+    const paginationOptions = [{
+      value: 0, label: this.$i18n.t('KFeaturesChart.ALL_VALUES')
+    }, {
+      value: 5, label: '5'
+    }, {
+      value: 10, label: '10'
+    }, {
+      value: 20, label: '20'
+    }]
+    const renderOptions = [{
+      value: 'value', label: this.$i18n.t('KFeaturesChart.VALUE_LABEL')
+    }, {
+      value: 'percentage', label: this.$i18n.t('KFeaturesChart.PERCENTAGE_LABEL')
+    }]
+
     return {
       toolbar: [{ name: 'close', icon: 'close', handler: () => this.close() }],
       property: {},
-      chartType: {},
-      chartTypes: ['pie', 'polarArea', 'radar', 'bar'].map(
-        type => ({ value: type, label: this.$i18n.t(`KFeaturesChart.CHART_LABEL_${type.toUpperCase()}`) })),
+      chartType: _.find(chartOptions, { value: 'pie' }),
+      chartOptions,
       currentChart: 1,
-      nbValuesPerChart: 0,
-      paginationOptions: [{
-        value: 0, label: this.$i18n.t('KFeaturesChart.ALL_VALUES')
-      }, {
-        value: 5, label: '5'
-      }, {
-        value: 10, label: '10'
-      }, {
-        value: 20, label: '20'
-      }],
+      nbValuesPerChart: _.find(paginationOptions, { value: 10 }),
+      paginationOptions,
+      renderOptions,
+      render: _.find(renderOptions, { value: 'value' }),
       chartData: []
     }
   },
@@ -104,16 +123,20 @@ export default {
       // Get possible values
       this.values = await this.getPropertyValues()
       // Then count features for each value
-      const responses = await Promise.all(this.values.map(value => {
-        return this.$api.getService('features', this.contextId)
+      let data = await Promise.all(this.values.map(async value => {
+        const response = await this.$api.getService('features', this.contextId)
           .find({ query: { $limit: 0, layer: this.layer._id, [`properties.${this.property.value}`]: value.value } })
+        return { value, count: response.total }
       }))
-      this.chartData = responses.map(response => response.total)
+      // Sort data so that we don't have charts mixin large and small numbers when paginating, go large first
+      data = _.sortBy(data, item => -item.count)
+      this.values = data.map(item => item.value)
+      this.chartData = data.map(item => item.count)
     },
     getChartOptions (type) {
-      const start = (this.currentChart - 1) * this.nbValuesPerChart
-      const end = (this.nbValuesPerChart > 0 ? start + this.nbValuesPerChart : this.chartData.length)
-      const colors = chroma.scale('Spectral').colors(end - start)
+      const start = (this.currentChart - 1) * this.nbValuesPerChart.value
+      const end = (this.nbValuesPerChart.value > 0 ? start + this.nbValuesPerChart.value : this.chartData.length)
+      const colors = _.shuffle(chroma.scale('Spectral').colors(end - start))
       let config = {
         type,
         data: {
@@ -160,11 +183,14 @@ export default {
         }
         default:
           _.set(config, 'data.datasets[0].backgroundColor', colors)
-          // When we display only a part of the data computing proportion is not relevant
-          _.set(config, 'options.plugins.labels.render', (this.nbValuesPerChart > 0 ? 'value' : 'percentage'))
+          _.set(config, 'options.plugins.labels.render', this.render.value)
+          _.set(config, 'options.plugins.labels.position', 'border')
+          _.set(config, 'options.plugins.labels.overlap', false)
+          _.set(config, 'options.plugins.labels.showActualPercentages', true)
           _.set(config, 'options.plugins.labels.precision', 0)
-          _.set(config, 'options.plugins.labels.fontSize', 12)
-          _.set(config, 'options.plugins.labels.fontColor', '#000')
+          _.set(config, 'options.plugins.labels.textShadow', true)
+          _.set(config, 'options.plugins.labels.fontSize', 24)
+          _.set(config, 'options.plugins.labels.fontColor', (type === 'bar' ? '#000' : '#fff'))
       }
 
       return config
@@ -176,9 +202,14 @@ export default {
       }
       // Retrieve data
       await this.getChartData()
-      // Setup the chart
+      // We need to force a refresh so that the computed props are correctly updated by Vuejs
+      await this.$nextTick()
       this.chart = new Chart(this.$refs.chart.getContext('2d'),
         this.getChartOptions(this.chartType.value))
+    },
+    async refreshChartAndPagination () {
+      this.currentChart = 1
+      await this.refreshChart()
     }
   },
   created () {
@@ -190,3 +221,11 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.chart {
+  border: solid 1px lightgrey;
+  border-radius: 8px;
+  background: #ffffff
+}
+</style>
